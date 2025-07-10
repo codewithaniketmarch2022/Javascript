2c4ab5ff4030362f07f4e16ce497096af0879737
aniket nalavade <aniketnalavade0538@gmail.com>
2025-07-11 00:16:06 +0530

    initial commit

---
+++ unit2.js
@@ -0,0 +1,15 @@
+
+let x=10
+console.log(x)
+
+//let x=20  //redeclaration is not allowed.....
+//console.log(x)
+
+ x=20;           //allowed updating a value.....
+console.log(x)
+
+if(true){
+    let y=30;
+    console.log(y)
+}
+    console.log(y)    //it cannot be accessed outside of block get an error
---
+++ unit3.js
@@ -0,0 +1,13 @@
+
+
+const PI = 3.14;
+console.log(PI)
+
+//PI = 3.1415;  //reassign not allowed..got an error
+//console.log(PI) 
+
+if(true){
+      const b=32.10;
+      console.log(b)   //got output because of having a block scope
+}
+     //console.log(b)  got error because not access outside of block
