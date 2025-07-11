ac152c44195a8075498a8f71fb7bb6b2679ed005
aniket nalavade <aniketnalavade0538@gmail.com>
2025-07-11 16:49:26 +0530

    initial commit

---
+++ NonPrimitiveType.js
@@ -0,0 +1,24 @@
+
+//Object
+let display = {
+    type: "Company",
+    location: "mumbai"
+}
+console.log(display.type)
+
+//An Array is a special kind of object used to store an ordered collection of values, which can be of any data type.
+let a1 = [1, 2, 3, 4, 5];
+console.log(a1)
+console.log(2)
+
+let a2 = [1, "two", { name: "Object" }, [3, 4, 5]];
+console.log(a2)
+console.log(3)
+
+//Function
+function greet(name) { return "Hello, " + name + "!"; }
+console.log(greet("Aniket"))
+
+//Date
+let today = new Date();
+console.log(today)
---
+++ PrimitiveDataTypes.js
@@ -0,0 +1,40 @@
+ 
+ //number data type....
+let p1 = 10;
+console.log(p1)
+
+let p2 = 1.3;
+console.log(p2)
+
+let p3 = Infinity;
+console.log(p3)
+
+let p4 = 'something here too' / 2;  // NaN represent infinite values and computational errors......
+console.log(p4)
+
+
+//String data types.....
+let s1 = "Health First Priority.....";
+console.log(s1)
+let s2 = 'Mumbai....';
+console.log(s2)
+
+//Boolean data types
+let b1 = true;
+console.log(b1);  
+
+let b2 = false;
+console.log(b2);
+
+//Null data type
+let age = null;
+console.log(age)
+
+//undefined data type
+let k;
+console.log(k);
+
+//Symbol data type
+let v1 = Symbol("aniket");
+let v2 = Symbol("aniket");
+console.log(s1 == s2);            //They help create unique keys in objects
