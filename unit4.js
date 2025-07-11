05062ab4757536507221ab0e651df02f9bedf086
aniket nalavade <aniketnalavade0538@gmail.com>
2025-07-11 12:07:15 +0530

    javascript practice

---
+++ unit4.js
@@ -0,0 +1,22 @@
+
+ //number data type....
+
+let p1 = 10;
+console.log(p1)
+
+let p2 = 1.3;
+console.log(p2)
+
+let p3 = Infinity;
+console.log(p3)
+
+let p4 = 'something here too' / 2;  // NaN represent infinite values and computational errors......
+console.log(p4)
+
+
+//String data types.....
+let s1 = "Health First Priority.....";
+console.log(s1); 
+
+let s2 = 'Mumbai....';
+console.log(s2);
